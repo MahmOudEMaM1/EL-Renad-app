6dfc128b262cd176af6540c50a5bfdafbb5803e3
ENG.EMO <115878930+MahmOudEMaM1@users.noreply.github.com>
2025-06-26 20:17:15 +0300

    name update 1

--- src/app/trip-time-out/trip-time-out.component.ts
+++ src/app/trip-time-out/trip-time-out.component.ts
@@ -3,10 +3,11 @@ import { Trip_Time_Out } from './modal/trip-time-out.modal';
 import { TripTimeOutService } from './service/trip-time-out.service';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-trip-time',
-  imports: [NgFor, CommonModule, FormsModule],
+  imports: [NgFor, CommonModule, FormsModule,TranslateModule],
   templateUrl: './trip-time-out.component.html',
   styleUrl: './trip-time-out.component.scss'
 })
